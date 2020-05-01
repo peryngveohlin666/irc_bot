6e27ef8ac24cbf9e36a4b818f4ca80b53f969ca0
peryngveohlin666 <46087170+peryngveohlin666@users.noreply.github.com>
2020-05-01 14:13:12 +0100

    dc fixed

--- src/Main.java
+++ src/Main.java
@@ -24,7 +24,7 @@ public class Main {
             }
         }
         catch (Exception e){
-        System.out.println("Please format the values like ip + port afterwise if you would like the bot to join to a specific channel invite it over the irc server by writing <botname> <invite> <channelname>, you can also set the password by passing in a 3rd argument instead of the default one");
+        System.out.println("Please format the values like ip + port afterwise if you would like the bot to join to a specific channel invite it over the irc server by writing <botname> <invite> <channelname>, you can also set the password by passing in a 4th argument instead of the default one and the #channel as the 3rd argument");
         }
         Bot bot = new Bot();
         bot.connect(ip, port, password, channel);
